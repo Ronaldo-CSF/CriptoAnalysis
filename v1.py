cd45d88830d1e0443c9aeb2f5da3d50335b730ce
“Ronaldo-CSF <ro.filho98@gmail.com>
2025-01-26 10:08:05 -0300

    Primeira_Versao

---
+++ v1.py
@@ -0,0 +1,419 @@
+# %% [markdown]
+# # Previsão de preço de Ethereum e Solana um estudo comparativo de técnicas de Machine Learning
+
+# %% [markdown]
+# ## Objetivo
+# 
+# O presente notebook tem como objetivo demonstrar a implementação de um algoritmo de machine learnig 
+# que possa auxiliar a tomada de decisão na compra de criptoativos, por meio da análise de dados históricos
+# como preço e captalização de mercado, permitindo identificar padrões e prever movimentações de mercado.
+# 
+# Para tal será realizada uma análise comparativa da precisão de previsões dos preços das criptomoedas Ether e 
+# Sol, utilizando três técnicas distintas de aprendizado de máquina: Random Forest [RF], LSTM e GRU. Pretende-se avaliar a 
+# eficiência e acurácia de cada modelo nas previsões, por meio das métricas de Erro Quadrático Médio [MSE], Raiz do Erro 
+# Quadrático Médio [RMSE] e Erro Absoluto Médio [MAE]; proporcionando uma compreensão sobre o desempenho de cada abordagem.
+# 
+# 
+# ## Estrutura do Trabalho
+# 
+# 1. Coleta de Dados
+# 2. Análise Explorativa de Dados
+# 3. Pré-processamento dos Dados
+# 4. Aplicação de Técnicas de Machine Learning
+# 5. Validação e Teste dos Modelos
+# 6. Interpretação dos Resultados
+
+# %% [markdown]
+# ### 1. Coleta de Dados
+# Recolher dados históricos de preços de diferentes criptoativos, volume de negociação, entre outras variáveis relevantes.
+
+# %% [markdown]
+# Os dados utilizados nesse trabalho foram extraidos do notebook presente no Kaggle: [The Most 50 Popular Crypto Data](https://www.kaggle.com/datasets/kaanxtr/btc-price-1m?resource=download).
+
+# %% [markdown]
+# 
+# ### 2. Análise Explorativa de Dados:
+# Realizar uma análise inicial dos dados para entender melhor os padrões históricos e a correlação entre diferentes criptoativos.
+# Utilizar visualizações gráficas e estatísticas descritivas para identificar tendências e anomalias.
+# 
+
+# %%  In[0]: Importação de Pacotes e base de dados
+import pandas as pd  
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+# import sklearn
+
+eth_data = pd.read_csv("databases/ETHUSDT.csv")
+sol_data = pd.read_csv("databases/SOLUSDT.csv")
+
+# %%  In[1]: Visualização das 6 primeiras observações
+eth_data.head()
+
+# %% In[2]: Visualização das 6 primeiras observações
+sol_data.head() 
+
+# %% In[3]: Visualização das variáveis
+print(eth_data.keys(),sol_data.keys(), sep="\n\n")
+
+# %% In[4]: Selecionar as Variáveis a manter
+sol = sol_data[['timestamp', 'close']]
+eth = eth_data[['timestamp', 'close']]
+
+eth['timestamp'] = pd.to_datetime(eth['timestamp'])
+eth.set_index('timestamp', inplace=True)
+
+sol['timestamp'] = pd.to_datetime(sol['timestamp'])
+sol.set_index('timestamp', inplace=True)
+
+# %% In[5]: Criação de Objeto TimeSeries
+eth_ts = pd.Series(data=eth['close'].values,index=eth.index)
+print(eth_ts.head())  # Verifique os primeiros valores
+
+# %% In[6]: Criação de Objeto TimeSeries
+sol_ts = pd.Series(data=sol['close'].values, index=sol.index)
+print(sol_ts.head())  # Verifique os primeiros valores
+
+# In[7]: Grafico como serie de tempo usando Plotly (Selecione todos os comandos)
+plt.figure(figsize=(10, 6))
+plt.plot(sol_ts)
+plt.title('Cotacao de SOL - Serie Temporal')
+plt.xlabel('Timestamp')
+plt.ylabel('Fechamento (SOL_USD)')
+plt.show()
+
+# In[8]: Grafico como serie de tempo usando Plotly (Selecione todos os comandos)
+plt.figure(figsize=(10, 6))
+plt.plot(eth_ts)
+plt.title('Cotacao de ETHER - Serie Temporal')
+plt.xlabel('Timestamp')
+plt.ylabel('Fechamento (ETHER_USD)')
+plt.show()
+
+# ------------------------------------------------------------------------------
+
+# %% [markdown]
+# A PARTIR DESSE MOMENTO TODO O TRABALHO VAI SER DESENVOLVIDO PARA ETH, E POSTERIORMENTE AJUSTADO PARA INCLUIR SOL!
+
+# ------------------------------------------------------------------------------
+
+cripto = eth
+
+cripto['close'] = cripto['close'].ffill()
+
+# %% In[9]: Realizando o resamplying dos dados
+
+# 5 min
+cripto_5min = cripto.resample('5T').mean()
+print(cripto_5min)
+
+# 15 min
+cripto_15min = cripto.resample('15T').mean()
+print(cripto_15min)
+
+# 30 min
+cripto_30min = cripto.resample('30T').mean()
+print(cripto_30min)
+
+# 1 hora
+cripto_1hr = cripto.resample('1H').mean()
+print(cripto_1hr)
+
+# 1 dia
+cripto_1dia = cripto.resample('1D').mean()
+print(cripto_1dia)
+
+# cripto_5min = pd.Series(data=cripto_5min['close'].values, index=cripto_5min.index)
+# cripto_15min = pd.Series(data=cripto_15min['close'].values, index=cripto_15min.index)
+# cripto_30min = pd.Series(data=cripto_30min['close'].values, index=cripto_30min.index)
+# cripto_1hr = pd.Series(data=cripto_1hr['close'].values, index=cripto_1hr.index)
+# cripto_1dia = pd.Series(data=cripto_1dia['close'].values, index=cripto_1dia.index)
+
+
+# %% [markdown]
+
+# >Ao analisar as colunas formadas observa-se a presença de valores faltantes, o que prejudica a analise e  
+# impede a continuação do código. Como os dados utilizados são de cotações, a ultima cotação existente é o  
+# último preço do ativo. Essa é a própria forma como o preço é apresentado. Assim, iremos preencher os valores  
+# com o último valor presente e as colunas de volume e numero de transações com zero.
+
+
+# In[10]: Preencher Missing Values
+
+# lista de resamples
+resample_cripto = (cripto_5min, cripto_15min, cripto_30min, cripto_1hr, cripto_1dia)
+
+for df in resample_cripto:
+    # Preenchendo a 'close' com o último valor válido
+    df["close"] = df["close"].fillna(method='ffill')
+
+
+# In[11]: Decomposição de Séries Temporais
+
+# Decomposicao pelo modelo ADITIVO
+from statsmodels.tsa.seasonal import seasonal_decompose
+cripto_decomp = seasonal_decompose(cripto_1hr["close"], model='aditive', period=24*30*12)
+
+# observando os valores da decomposicao pelo modelo aditivo
+print(cripto_decomp.trend)
+print(cripto_decomp.seasonal)
+print(cripto_decomp.resid)
+
+
+# In[12]: Plotar a decomposicao (Selecionar todos os comandos)
+plt.figure(figsize=(10, 8))
+plt.subplot(4, 1, 1)
+plt.plot(cripto_decomp.trend)
+plt.title('Tendencia')
+
+plt.subplot(4, 1, 2)
+plt.plot(cripto_decomp.seasonal)
+plt.title('Componente Sazonal')
+
+plt.subplot(4, 1, 3)
+plt.plot(cripto_decomp.resid)
+plt.title('Residuos')
+
+plt.subplot(4, 1, 4)
+plt.plot(cripto_1hr["close"], label='Original')
+plt.plot(cripto_decomp.trend + cripto_decomp.seasonal + cripto_decomp.resid, label='Reconstruida')
+plt.title('Original vs. Reconstruida')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+# %% In[13]: Criação de Features Adicionais:
+
+# Random Forest não lida diretamente com a dependência temporal, como outros modelos
+# especializados em séries temporais (ARIMA, LSTM). Para contornar isso, é necessário 
+# criar variáveis explicativas (X) a partir de transformações nos dados, como janelas 
+# deslizantes, indicadores técnicos ou agregados temporais.
+
+for df in resample_cripto:
+
+    # Retornos percentuais (% variação do preço).
+    df['return_pct'] = df['close'].pct_change() * 100
+
+    # Médias móveis (curta e longa duração).
+    df['SMA_10'] = df['close'].rolling(window=10).mean()  # Média móvel simples (janela de 10 períodos)
+    df['EMA_10'] = df['close'].ewm(span=10, adjust=False).mean()  # Média móvel exponencial (janela de 10 períodos)
+
+    # Índice de Força Relativa (RSI - Relative Strength Index)
+    delta = df['close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()  # Ganhos médios
+    loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()  # Perdas médias
+    rs = gain / loss
+    df['RSI'] = 100 - (100 / (1 + rs))
+
+    df.dropna(inplace=True)
+
+
+## ---------------------TESTE COM DADOS Diários --------------------- ##
+
+# %% In[14]: Segregar o dataset em dados de treino, validação e teste
+
+df = cripto_1dia
+
+# Importação de bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Proporções
+train_size = int(len(df) * 0.7)  # 70% para treino
+val_size = int(len(df) * 0.2)    # 20% para validação
+
+# Divisão em treino, validação e teste
+train = df[:train_size]
+val = df[train_size:train_size + val_size]
+test = df[train_size + val_size:]
+
+# Verificando as divisões
+print(f'Tamanho treino: {len(train)}, validação: {len(val)}, teste: {len(test)}')
+
+
+# Preparação dos dados
+X_train, y_train = train.drop(columns=['close']), train['close']
+X_val, y_val = val.drop(columns=['close']), val['close']
+X_test, y_test = test.drop(columns=['close']), test['close']
+
+
+# %% In[15]: Aplicar a normalização dos dados de treino do modelo
+
+from sklearn.preprocessing import StandardScaler
+
+# Escalonar apenas o conjunto de treino
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+
+# Usar o mesmo escalador nos outros conjuntos
+X_val_scaled = scaler.transform(X_val)
+X_test_scaled = scaler.transform(X_test)
+
+# ------------------------------------------------------------------------------------------------ #
+# %% In[16]: Aplicar o Algoritmo de Return-Based Naive Model usado como benchmark
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Fazer previsões no conjunto de validação e teste usando o modelo de persistência com retornos
+val['return_BM'] = train['return_pct'].iloc[-1] / 100  # Use o último retorno conhecido no treino
+val['predicted_price_BM'] = val['close'].shift(1) * (1 + val['return_BM'])
+
+test['return_BM'] = val['return_pct'].iloc[-1] / 100  # Use o último retorno conhecido na validação
+test['predicted_price_BM'] = test['close'].shift(1) * (1 + test['return_BM'])
+
+# Calcular o erro (exemplo: MAE)
+mae_benchmark_val = mean_absolute_error(val['close'].iloc[1:], val['predicted_price_BM'].iloc[1:])
+print(f'MAE_val do Benchmark: {mae_benchmark_val: .2f}')
+mae_benchmark_test = mean_absolute_error(test['close'].iloc[1:], test['predicted_price_BM'].iloc[1:])
+print(f'MAE_test do Benchmark: {mae_benchmark_test: .2f}')
+
+mse_val = mean_squared_error(y_val, y_val_pred)
+rmse_val = np.sqrt(mse_val)
+
+
+# %% In[17]: Aplicar o Algoritmo de Random Forest
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# %% Para o conjunto de validação
+# Modelo Random Forest
+rf_model = RandomForestRegressor(n_estimators=300, random_state=42)
+
+# Treinamento
+rf_model.fit(X_train_scaled, y_train)
+
+# Previsão no conjunto de validação
+y_val_pred = rf_model.predict(X_val_scaled)
+
+# Avaliação no conjunto de validação
+mae_val = mean_absolute_error(y_val, y_val_pred)
+mse_val = mean_squared_error(y_val, y_val_pred)
+rmse_val = np.sqrt(mse_val)
+
+print(f'MAE validação: {mae_val:.2f}')
+print(f'MSE validação: {mse_val:.2f}')
+print(f'RMSE validação: {rmse_val:.2f}')
+
+# %% In[18]: Visualizando os resultados de validação:
+
+plt.figure(figsize=(10, 6))
+
+# Plotando os valores reais com as datas no eixo x
+plt.plot(y_val.index, y_val, label='Valores Reais', color = 'blue')
+
+# Plotando as previsões com as mesmas datas
+plt.plot(y_val.index, y_val_pred, label='Previsões', color = 'orange')
+
+# Ajustando o gráfico
+plt.legend()
+plt.title('Valores Reais vs Previsões (Random Forest)')
+plt.xlabel('Data')  # Rótulo do eixo x
+plt.ylabel('Valor de Fechamento')  # Rótulo do eixo y
+plt.grid(True)
+
+# Rotação das datas para melhor visualização
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
+
+
+# %% In[19]: Avaliação no Conjunto de Teste
+# Após ajustar o modelo com os dados de validação, avaliamos no conjunto de teste para verificar a generalização:
+
+y_test_pred = rf_model.predict(X_test_scaled)
+
+# Avaliação
+mae_test = mean_absolute_error(y_test, y_test_pred)
+mse_test = mean_squared_error(y_test, y_test_pred)
+rmse_test = np.sqrt(mse_test)
+
+print(f'MAE teste: {mae_test:.2f}')
+print(f'MSE teste: {mse_test:.2f}')
+print(f'RMSE teste: {rmse_test:.2f}')
+
+
+
+# %% In[20]: Testando a combinação dos gráficos
+
+# Previsões completas: treino, validação e teste
+y_train_pred = rf_model.predict(X_train_scaled)
+y_val_pred = rf_model.predict(X_val_scaled)
+y_test_pred = rf_model.predict(X_test_scaled)
+
+# Concatenando os valores reais e previstos
+y_real = pd.concat([y_train, y_val, y_test])
+y_pred = pd.concat([
+    pd.Series(y_train_pred, index=y_train.index),
+    pd.Series(y_val_pred, index=y_val.index),
+    pd.Series(y_test_pred, index=y_test.index)
+])
+
+# Gráfico comparativo
+plt.figure(figsize=(12, 6))
+
+# Série temporal original (valores reais)
+plt.plot(y_real.index, y_real.values, label='Série Original (Valores Reais)', color='blue', alpha=0.7)
+
+# Previsões (treino + validação + teste)
+plt.plot(y_pred.index, y_pred.values, label='Previsões (Treino + Validação + Teste)', color='orange', alpha=0.7)
+
+# Ajustando o gráfico
+plt.legend()
+plt.title('Série Temporal: Original vs Previsões (Random Forest)')
+plt.xlabel('Data')
+plt.ylabel('Valor de Fechamento')
+plt.grid(True)
+
+# Rotação das datas para melhor visualização
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
+
+
+# %% [markdown]
+# ### 3. Pré-processamento dos Dados: 
+# Limpeza dos dados, tratamento de valores ausentes e normalização dos dados para garantir a qualidade e consistência das análises subsequentes.
+
+# %% [markdown]
+# ### 4. Aplicação de Técnicas de Machine Learning:
+# Modelos de Regressão: Utilizar modelos como Regressão Linear para prever preços futuros com base em tendências históricas.
+# Modelos de Classificação: Aplicar algoritmos como Random Forest e SVM para classificar os períodos de alta e baixa no mercado.
+# 
+
+# %% [markdown]
+# ### 5. Validação e Teste dos Modelos:
+# Dividir os dados em conjuntos de treinamento e teste para validar a eficácia dos modelos.
+# Utilizar técnicas de validação cruzada para garantir a robustez dos resultados.
+# 
+
+# %% [markdown]
+# ### 6. Interpretação dos Resultados:
+# Analisar o desempenho dos modelos, identificando quais técnicas apresentam melhores resultados na previsão de ciclos de mercado.
+# Discussão sobre as limitações do estudo e possíveis melhorias para pesquisas futuras.
+
+
+
+
+
+
+
+# Inicialmente proposto:
+#    1. Base de dados:
+#       sol = sol_data[['timestamp', 'close', 'volume', 'number_of_trades']]
+#    2. Resample funcions:
+#       cripto_5min = cripto.resample('5T').agg({'close': 'mean', 'volume': 'sum', 'number_of_trades': 'sum'})
+#    3. Criação do Objeto Serie do Resample:
+#       cripto_close_5m = pd.Series(data=cripto_5min['close'].values, index=cripto_5min.index)
+#    4. Preencher Missing Value:
+#       for df in resample_cripto:
+#           df[["volume", "number_of_trades"]] = df[["volume", "number_of_trades"]].fillna(0)
+#    5. Preparação dos dados para Random Forest 
+#       X = cripto_1hr_stand[['volume', 'number_of_trades', 'return_pct', 'SMA_10', 'EMA_10', 'RSI']] 
+
